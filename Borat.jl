57b089775a5d6cc5d0a79aa9d26fff23c1002358
Yakir Gagnon <12.yakir@gmail.com>
2014-09-04 20:48:16 +1000

    cleaned the comments

--- Borat.jl
+++ Borat.jl
@@ -1,57 +1,56 @@
-using PyPlot, Shapes, RayTrace
+using PyPlot, Shapes, RayTrace # PyPlot is slow and in the ultimate version will be removed
 
-const ε = 1e-4 # the ε size with which the ray bend every iteration
-const lens_r = 1. # the lens radius, this is only useful cause there are a couple of calculations that depend on this variable
-const maxiter = int(2π*lens_r/ε)
-const c = [100.,-20.,330.] # this is the location of the center of the lens
-const retina_r = 1.2*lens_r # this is the retina's radius, good practice to make it a function of the lens radius 
-const L = Inf # this is the distance between the center of the lens and the source light
-const nrays = 100 # the number of discrete rays I'll be tracing
-const n_medium = 1. # this is the refractive index of the medium surrounding the lens
-const lens_r2 = lens_r*lens_r
-const goal = c .- [0.,0.,lens_r]
-ri(r::Float64) = sqrt(2. - r*r/lens_r2) # this is the RIG function. Notice that its argument is now Real -- this is in the interest of generality... I set this RIG to be equal to the Luneburg lens, so that we can check the results real quick. see: http://en.wikipedia.org/wiki/Luneburg_lens
-
-#=OK, now comes the actual calculations=#
-const lens = Lens(c,lens_r,ri,ε) # initiate the lens, this is how we initiate an instance of a type, it's like a function!
+# constants:
+const ε = 1e-4 # the ε size with which the ray advances every iteration
+const lens_r = 1. # the lens radius
+const maxiter = int(2π*lens_r/ε) # maximum iteration allowed. This value is based on the arc-length of an imaginary ray that travels along the surface of the lens, times two! So should be safe.
+const c = [100.,-20.,330.] # location of the center of the lens. It has no real use here, but good to have it.
+const retina_r = 1.2*lens_r # the retina's radius, good practice to make it a function of the lens radius 
+const L = Inf # the distance between the center of the lens and the source light
+const nrays = 100 # the number of discrete rays
+const n_medium = 1. # the refractive index of the medium surrounding the lens
+const lens_r2 = lens_r*lens_r # just to make the refractive index gradient function faster
+const goal = c .- [0.,0.,lens_r] # this is where all rays that start at L=Inf should end up at.
+ri(r::Float64) = sqrt(2. - r*r/lens_r2) # this is the refractive index gradient function. I set this RIG to be equal to the Luneburg lens, so that we can check the results real quick.
+const lens = Lens(c,lens_r,ri,ε) # initiate the lens
 const retina = Retina(c,retina_r) # initiate the retina
 
-P = zeros(nrays)
-nsteps = 0
-#=Now we iterate through all the rays, tracing them=#
+# actual calculations
+Δ = zeros(nrays) # the matrix of euclidean distance between where the ray exsits the lens and the goal
+# iterate through all the rays, ploting them
 for i = 1:nrays
-    p = zeros(3,maxiter)
-    diagnose = [:Δ => 0., :len => 0, :goal => goal]
-    trace!(p,lens,retina,L,n_medium,maxiter,diagnose)
-    P[i] = diagnose[:Δ]
-    ind = round(linspace(2,diagnose[:len],10))
-    plot3D(vec(p[1,ind]),vec(p[2,ind]),vec(p[3,ind]),color="red")
+    p = zeros(3,maxiter) # ray locations
+    diagnose = [:Δ => 0., :len => 0, :goal => goal] # diagnosys, including the distance to the goal, the number of refraction points, the goal
+    trace!(p,lens,retina,L,n_medium,maxiter,diagnose) # main function that ray-traces the system
+    Δ[i] = diagnose[:Δ] # put that distance in the matrix
+    # plotting:
+    ind = round(linspace(2,diagnose[:len],10)) # let's plot just 10 points inside the lens, no need to go crazy
+    plot3D(vec(p[1,ind]),vec(p[2,ind]),vec(p[3,ind]),color="red") # plotting it in red...
 end
 
-#>now comes the shitty sphere plot. I think I improved it a bit :)<] =#
+# sphere plot
 n = 100 # number of points to plot=#
-u = linspace(0., π, n) # the two angles that define the location of the points on the sphere=#
-v = linspace(0., 2π, n)' # notice that I transpose this one with '=#
-u .+= 0*v # the transposition is "carried" and expanded through the .+ operator to make u a n by n matrix=#
-v .+= 0*u # same here=#
-x = lens.c.x + lens.r*cos(u).*sin(v) # then standard vector math=#
+u = linspace(0., π, n) # the two angles that define the location of the points on the sphere
+v = linspace(0., 2π, n)' # notice that I transpose this one with '
+u .+= 0*v # the transposition is "carried" and expanded through the .+ operator to make u a n by n matrix
+v .+= 0*u # same here
+x = lens.c.x + lens.r*cos(u).*sin(v) # then standard vector math
 y = lens.c.y + lens.r*sin(u).*sin(v)
 z = lens.c.z + lens.r*cos(v)
-plot_surface(x, y, z, color="blue",alpha=.5,linewidth=0,rstride=4,cstride=4) # and finally plotting the sphere with some transparency so that we'll be able to see the ray=#
+plot_surface(x, y, z, color="blue",alpha=.5,linewidth=0,rstride=4,cstride=4) # and finally plotting the sphere with some transparency so that we'll be able to see the ray
 
-#>This is for plotting a part of the retina, like a hemisphere, but not hemi... This code is exactly the same as for the initiation of the rays, when they are gonna hit the lens<]=#
-θ = .5 # this is the angle of the sphere-retina that will be plotted=#
+# plotting a part of the retina, like a hemisphere, but not hemi... This code is exactly the same as for the initiation of the rays, when they are gonna hit the lens
+θ = .5 # this is the (arbitrary) angle of the sphere-retina that will be plotted
 
-#>z and h are uniformly sampled from those ranges to generate the uniformly spaced points on the sphere<]=#
+# z and h are uniformly sampled from these ranges to generate the uniformly spaced points on the sphere
 z = linspace(cos(θ),1.,n)
 h = linspace(0.,2π,n)'
 
-#>now come the x, y, and z that lie on the retina's surface<]=#
+# now come the x, y, and z that lie on the retina's surface
 x = retina.c.x + retina.r*sqrt(1. - z.^2).*cos(h)
 y = retina.c.y + retina.r*sqrt(1. - z.^2).*sin(h)
-z = retina.c.z - retina.r*z .+ 0.*h # I flip the z so it'll point downwards=#
+z = retina.c.z - retina.r*z .+ 0.*h # I flip the z so it'll point downwards
 
-plot_surface(x, y, z, color="green",alpha=.25,linewidth=0,rstride=4,cstride=4) # and finally plotting the sphere with some transparency so that we'll be able to see the ray=#
+plot_surface(x, y, z, color="green",alpha=.25,linewidth=0,rstride=4,cstride=4) # and finally plotting the sphere with some transparency
 
-#>Thought I'd assess the accuracy of the focal point. The points in p one before last are all at the lens surface, and because the rig is a Luneburg lens, then they should all be lens radius below the lens origin. Feel free to improve on the following:<]=#
-σ = mean(P)
+σ = mean(Δ) # mean error
